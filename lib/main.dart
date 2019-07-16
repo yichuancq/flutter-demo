c733dfa30f40ff0c381d81586c4dbd2c790e8237
yichuancq <1012027293@qq.com>
2019-07-16 14:50:25 +0800

    add  resource 文件

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'adapter/SampleAppPageTest.dart';
+//import 'package:untitled/listview/RandomWords.dart';
+import 'listview/RandomWords.dart';
+import 'utils/const.dart';
 // main 为程序的入口 通过 runApp() 方法执行我们的 class
 void main() => runApp(new MyApp());
 /* 创建一个无状态的 Widget */
@@ -9,9 +11,10 @@ class MyApp extends StatelessWidget {
     // 创建一个 material 风格的 app
     return new MaterialApp(
       title: 'Demo',    // 设置 App 的标题
-      theme: new ThemeData(primaryColor: Colors.teal), //设置主题颜色
+      theme: Constants.lightTheme,
+     // theme: new ThemeData(primaryColor: Colors.teal), //设置主题颜色
       debugShowCheckedModeBanner: false, // 去掉DEBUG 右上角 设置这一属性即可
-      home: new SampleAppPage()
+      home: new RandomWords()
       // home: new TabBarDemo(),
     );
   }
---
+++ lib/utils/data.dart
@@ -0,0 +1,27 @@
+//自定义list
+List restaurants = [
+  {
+    "img": "assets/head1.png",
+    "title": "Happy Jones",
+    "address": "1278 Loving Acres RoadKansas City, MO 64110",
+    "rating": "4.5"
+  },
+  {
+    "img": "assets/head1.png",
+    "title": "Uncle Boons",
+    "address": "1278 Loving Acres RoadKansas City, MO 64110",
+    "rating": "4.5"
+  },
+  {
+    "img": "assets/head1.png",
+    "title": "Happy Jones",
+    "address": "1278 Loving Acres RoadKansas City, MO 64110",
+    "rating": "4.5"
+  },
+  {
+    "img": "assets/head1.png",
+    "title": "Uncle Boons",
+    "address": "1278 Loving Acres RoadKansas City, MO 64110",
+    "rating": "4.5"
+  }
+];
